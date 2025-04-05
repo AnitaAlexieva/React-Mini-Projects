9fef45e2dbe4b54bce842742d66de73bc12114c3
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-05 23:51:17 +0300

    Create header component

---
+++ To-Do-LIst-App/my-app/src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <section className="header">
+            <h1>Hello! I am here to help you organize you daily task!</h1>
+            <h3>Here yoy can create, delete, or cross a task</h3>
+        </section>
+    )
+}

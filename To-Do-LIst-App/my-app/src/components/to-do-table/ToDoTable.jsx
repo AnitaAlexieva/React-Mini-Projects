28220e919cb71a57da2e2e0f56990925c3d0ae79
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-06 01:13:19 +0300

    Create To Do List Table

---
+++ To-Do-LIst-App/my-app/src/components/to-do-table/ToDoTable.jsx
@@ -0,0 +1,25 @@
+export default function ToDoTable() {
+    return(
+        <div className='to-do-table'>
+        <table>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Yes</td>
+              <td>No</td>
+            </tr>
+            <tr>
+              <td>Name</td>
+              <td>Yes</td>
+              <td>No</td>
+            </tr>
+            <tr>
+              <td>Name</td>
+              <td>Yes</td>
+              <td>No</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+}

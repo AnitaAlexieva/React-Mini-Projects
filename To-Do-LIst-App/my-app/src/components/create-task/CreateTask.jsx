318b344903b518a06eb10cf465fae0ace65d9828
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-06 23:29:10 +0300

    Improve CreateTask design

--- To-Do-LIst-App/my-app/src/components/create-task/CreateTask.jsx
+++ To-Do-LIst-App/my-app/src/components/create-task/CreateTask.jsx
@@ -2,7 +2,7 @@ export default function CreateTask() {
     return (
         <div className='create-form'>
             <form>
-                <input className="create-input" type="text" name='newTask' placeholder='I have to...'></input>
+                <input className="create-input" type="text" name='newTask' placeholder=' I have to...'></input>
                 <button className="add-btn" type='submit'>Add Task</button>
             </form>
         </div>

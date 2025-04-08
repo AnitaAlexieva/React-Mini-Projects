c5206631239f6955e9d68d8790022aee5ddbb142
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-08 23:51:21 +0300

    Create task

--- To-Do-LIst-App/my-app/src/components/create-task/CreateTask.jsx
+++ To-Do-LIst-App/my-app/src/components/create-task/CreateTask.jsx
@@ -1,9 +1,20 @@
+import { useCreateTask } from "../../api/taskApi";
+
+
 export default function CreateTask() {
+
+    const {create} = useCreateTask();
+
+    const createHadnler = async(formData) =>{
+        const data = Object.fromEntries(formData);
+        const task = await create(data);
+        console.log(task);
+    }
     return (
         <div className='create-form'>
             <form>
                 <input className="create-input" type="text" name='newTask' placeholder=' I have to...'></input>
-                <button className="add-btn" type='submit'>Add Task</button>
+                <button className="add-btn" type='submit' onSubmit={createHadnler}>Add Task</button>
             </form>
         </div>
     )

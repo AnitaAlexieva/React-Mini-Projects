48395f4ea6758d6f5dbeb95eb61a1e81065a593d
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-10 22:48:01 +0300

    Made small updates

--- To-Do-LIst-App/my-app/src/components/header/Header.jsx
+++ To-Do-LIst-App/my-app/src/components/header/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <section className="header">
             <h1>Hello! Let's help you organize your daily tasks!</h1>
-            <h3>Here you can create, delete, or cross a task!</h3>
+            <h3>Here you can create, delete or cross a task!</h3>
         </section>
     )
 }

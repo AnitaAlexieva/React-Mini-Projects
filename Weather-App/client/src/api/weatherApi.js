912f17af37eba902cccb32fb75f2dc83ea6e7890
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-15 00:37:27 +0300

    Fix CORS issue when fetching weather data

--- Weather-App/client/src/api/weatherApi.js
+++ Weather-App/client/src/api/weatherApi.js
@@ -1,11 +1,10 @@
 import { requester } from "../utils/requester"
 
-
-export const  getWreather = async (cityName) =>{
-    const data = await requester(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=443e51215c2f4f0eaf705b1659b6d924&units=metric`,
+export const getWreather = async (cityName) => {
+    const data = await requester(
+        `/weather?q=${cityName}&appid=443e51215c2f4f0eaf705b1659b6d924&units=metric`,
         "GET"
-    )
+    );
 
-    const weather = await data.json();
-    return weather;
-}
+    return data;
+};

c63e31b44a6b3f1e936ab5cb29f0e22eb0dbbc75
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-16 10:57:34 +0300

    Add read more functionality

--- Weather-App/client/src/components/WeatherCard.jsx
+++ Weather-App/client/src/components/WeatherCard.jsx
@@ -5,6 +5,7 @@ export default function WeatherCard() {
 
     const [city, setCity] = useState('Plovdiv')
     const [weather, setWeather] = useState(null)
+    const [isClicked, setIsClicked] = useState(false);
 
     useEffect(() => {
         const fetchWeather = async () => {
@@ -24,6 +25,10 @@ export default function WeatherCard() {
         setCity(cityName);
         console.log(city)
     }
+    if (!weather) {
+        return <p>Loading weather...</p>;
+    }
+   
 
     return (
         <>
@@ -32,9 +37,7 @@ export default function WeatherCard() {
                 <button className="search-btn" type="submit">Search</button>
             </form>
 
-
-
-            {weather ? (
+            {!isClicked ? (
                 <div className="weather-card">
                     <h3 className="weather-h3">Weather in {city}</h3>
                     <div className="weather-info">
@@ -52,11 +55,34 @@ export default function WeatherCard() {
 
                     </div>
                     <div className="btn-container">
-                        <button className="more-btn">Read More</button>
+                        <button className="more-btn" onClick={()=> setIsClicked(true)}>Read More</button>
                     </div>
                 </div>
             ) : (
-                <p>Loading weather...</p>
+                <div className="weather-card">
+                    <h3 className="weather-h3">Weather in {city}</h3>
+                    <div className="weather-info">
+                        <div className="weather-text">
+                            <p>Tempreture: {weather.main.temp} °C</p>
+                            <p>Feels like: {weather.main.feels_like} °C</p>
+                            <p>Min tempreture: {weather.main.temp_min}</p>
+                            <p>Max tempreture: {weather.main.temp_max}</p>
+                            <p>Clouds: {weather.clouds.all}</p>
+                            <p>Weather description: {weather.weather[0].description}</p>
+                            <p>Wind speed: {weather.wind.speed} km/h</p>
+                        </div>
+                        <div className="weather-icon">
+                            <img
+                                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                                alt="Weather icon"
+                            />
+                        </div>
+
+                    </div>
+                    <div className="btn-container">
+                        <button className="more-btn" onClick={()=> setIsClicked(false)}>Show Less</button>
+                    </div>
+                </div>
             )}
 
         </>

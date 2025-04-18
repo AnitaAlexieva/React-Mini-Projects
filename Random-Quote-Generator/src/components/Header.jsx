44bdb78eed7ddb97873bb6dba400f29e2e7dfe6a
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-18 09:02:36 +0300

    Create Header component

---
+++ Random-Quote-Generator/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div className='app-wrapper'>
+            <div className="header-div">
+                <p className="p-heading">Hello, let's see your quote for today!</p>
+            </div>
+
+        </div>
+    )
+}

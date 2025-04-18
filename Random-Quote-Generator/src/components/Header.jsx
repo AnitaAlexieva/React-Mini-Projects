584c65ed4b9e9eefe5303b7e46ae97be8f36dae3
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-18 23:30:04 +0300

    Add quote component to header component

--- Random-Quote-Generator/src/components/Header.jsx
+++ Random-Quote-Generator/src/components/Header.jsx
@@ -1,10 +1,12 @@
+import Quote from "./Quote";
+
 export default function Header() {
     return (
         <div className='app-wrapper'>
             <div className="header-div">
                 <p className="p-heading">Hello, let's see your quote for today!</p>
             </div>
-
+           <Quote/>
         </div>
     )
 }
